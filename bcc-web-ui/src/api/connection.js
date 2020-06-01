644f71ca78b15c39d4a4d9073278add8e0eab456
a0906101 <a0906101@unet.univie.ac.at>
2020-06-01 21:33:00 +0200

    url

--- bcc-web-ui/src/api/connection.js
+++ bcc-web-ui/src/api/connection.js
@@ -3,7 +3,7 @@ const moment = require('moment')
 const Web3 = require('web3')
 const web3 = new Web3()
 
-const baseURL = 'http://10.0.1.4/api'
+const baseURL = 'api'
 
 const APIGateCall = async (method, url, body) => {
   try {
